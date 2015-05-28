971a169f82a77d735dd906284f69b25935fd3044
Aleksandr Tsertkov <tsertkov@gmail.com>
2015-05-28 12:07:58 +0200

    Add examples

---
+++ examples/hellogo.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+DIR="$(mktemp -d "$(pwd)/build-XXXXXX")"
+cd "$DIR"
+
+cat > build.sh <<'EOT'
+GOPATH=$(pwd) go get github.com/golang/example/hello
+cp bin/hello /build/hello
+EOT
+
+docker run \
+  --rm \
+  -v ${DIR}:/build \
+  tsertkov/builder \
+
+if [ $? -ne 0 ]; then
+  rm -rf "$DIR"
+  echo "Build failed"
+  exit 1
+else
+  rm "${DIR}/build.sh"
+  echo "Build successful ${DIR}"
+fi
